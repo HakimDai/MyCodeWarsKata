c54052647fb00839f06706ef9307d70a82877140
HakimDai <hakim69400@live.com>
2020-08-29 23:21:25 +0200

    Find the odd int solution

---
+++ src/6 Kyu/6Kyu-Find the ood int.ts
@@ -0,0 +1,17 @@
+// Given an array of integers, find the one that appears an odd number of times.
+//
+//     There will always be only one integer that appears an odd number of times.
+
+export const findOdd = (xs: number[]) => {
+    for (let nb of xs) {
+        let count = 0;
+        for (let i = 0; i < xs.length; i++) {
+            if (nb === xs[i]) {
+                count++;
+            }
+        }
+        if (count % 2 !== 0) {
+            return nb;
+        }
+    }
+};
