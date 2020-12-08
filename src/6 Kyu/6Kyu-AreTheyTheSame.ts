7b819daa217a490eab51ff101b7bf2f0dd0218fc
DAIFALLAH Hakim <hakim.Daifallah@groupeadequat.fr>
2020-12-08 16:50:17 +0100

    6 kyu are they the same

---
+++ src/6 Kyu/6Kyu-AreTheyTheSame.ts
@@ -0,0 +1,21 @@
+// Given two arrays a and b write a function
+// comp(a, b) (compSame(a, b) in Clojure)
+// that checks whether the two arrays have the "same" elements,
+// with the same multiplicities.
+// "Same" means, here, that the elements in b are the elements in a squared
+// regardless of the order.
+
+export function comp(a1: number[] | null, a2: number[] | null): boolean {
+  if (a1 && a2) {
+    if (a1.every((value) => gotNotTheSquare(value, a2))) {
+      return false;
+    } else if (a1.every((value) => !gotNotTheSquare(value, a2))) {
+      return true;
+    };
+  }
+  return false;
+}
+
+const gotNotTheSquare = (value: number, values: number[]) => {
+  return values.every((arrayValue) => value * value !== arrayValue);
+};
