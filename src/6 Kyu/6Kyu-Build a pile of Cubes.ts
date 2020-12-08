1ce6164f0765da36a5fbc363bf3e45511be9563c
DAIFALLAH Hakim <hakim.Daifallah@groupeadequat.fr>
2020-12-08 16:50:17 +0100

    6 kyu are they the same

--- src/6 Kyu/6Kyu-Build a pile of Cubes.ts
+++ src/6 Kyu/6Kyu-Build a pile of Cubes.ts
@@ -1,9 +1,14 @@
-// Your task is to construct a building which will be a pile of n cubes. The cube at the bottom will have a volume of n^3,
-// the cube above will have volume of (n-1)^3 and so on until the top which will have a volume of 1^3.
+// Your task is to construct a building which will be a pile of n cubes.
+// The cube at the bottom will have a volume of n^3,
+// the cube above will have volume of (n-1)^3
+// and so on until the top which will have a volume of 1^3.
 //
-// You are given the total volume m of the building. Being given m can you find the number n of cubes you will have to build?
+// You are given the total volume m of the building.
+// Being given m can you find the number n of cubes you will have to build?
 //
-//     The parameter of the function findNb (find_nb, find-nb, findNb) will be an integer m and you have to return the integer n such as n^3 + (n-1)^3 + ... + 1^3 = m
+// The parameter of the function findNb (find_nb, find-nb, findNb)
+// will be an integer m
+// and you have to return the integer n such as n^3 + (n-1)^3 + ... + 1^3 = m
 //     if such a n exists or -1 if there is no such n.
 //
 //     Examples:
@@ -17,17 +22,17 @@
 
 let n = 0;
 export function findNb(m: number): number {
-    let result = 0;
-    n++;
-    let cube = Math.pow(n, 3);
-    if ((m - cube) < 0) {
-        n = 0;
-        return -1;
-    } else if ((m - cube) === 0) {
-        result = n;
-        n = 0;
-        return result;
-    } else {
-        return findNb((m - cube));
-    }
+  let result = 0;
+  n++;
+  const cube = Math.pow(n, 3);
+  if ((m - cube) < 0) {
+    n = 0;
+    return -1;
+  } else if ((m - cube) === 0) {
+    result = n;
+    n = 0;
+    return result;
+  } else {
+    return findNb((m - cube));
+  }
 }
